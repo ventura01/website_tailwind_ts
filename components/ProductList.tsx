ead891dea8ade1de47d847fc8a04c137535f4247
rafael <rventu@gmail.com>
2023-02-25 13:34:48 -0600

    Product card modified

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -11,24 +11,29 @@ const ProductList = () => {
             our products are solutions for yor needs
           </h2>
         </div>
-        <div className="grid grid-cols-3 ">
+        <div className="grid grid-cols-1 md:grid-cols-3 ">
           {products.map((product) => (
             <div
               key={product.id}
-              className="border border-gray-300 rounded-3xl gap-2 w-3/4 overflow-hidden"
+              className="border border-gray-300 rounded-3xl my-4 gap-2 w-full mx-auto overflow-hidden"
             >
               <Image
                 src={product.url}
                 alt={product.name}
                 width={600}
                 height={600}
-                className="object-cover"
+                className="object-cover h-64 w-full object-center"
               />
               <div className="p-4">
-                <h3 className="uppercase text-1xl font-bold text-gray-800">{product.name}</h3>
+                <h3 className="uppercase text-1xl font-bold text-gray-800">
+                  {product.name}
+                </h3>
                 <p className="text-gray-500">{product.desc}</p>
                 <p className="font-bold">U${product.decsPrice}</p>
                 <del>U${product.price}</del>
+              </div>
+              <div className="flex justify-center my-6 mt-4">
+                <button className="rounded-full hover:bg-orange-500 hover:text-white capitalize shadow-md shadow-orange-300 bg-white py-2 outline outline-orange-500 text-orange-500 font-bold px-6">shop now</button>
               </div>
             </div>
           ))}
